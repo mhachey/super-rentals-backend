1b6e6ca369e495939854a6b4a79a3eaed282531c
Mike Hachey <michael.g.hachey@gmail.com>
2022-04-25 10:48:15 -0400

    JSONAPI serializer for Rentals

---
+++ app/serializers/rental_serializer.rb
@@ -0,0 +1,14 @@
+class RentalSerializer
+  include JSONAPI::Serializer
+
+  set_type :rentals
+  set_id :url_slug
+
+  attributes :id, :title, :owner, :city, :category, :bedrooms, :image
+  attribute :location do |rental|
+    {
+      lat: rental.lat,
+      lng: rental.lng
+    }
+  end
+end
