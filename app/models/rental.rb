bac65d66422265f1375d8a7d57289cae33a60225
Mike Hachey <michael.g.hachey@gmail.com>
2022-04-25 11:23:44 -0400

    add database seed script

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,3 +1,2 @@
 class Rental < ApplicationRecord
-  attr_accessor :title, :owner, :city, :lat, :lng, :category, :bedrooms, :image, :url_slug
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+rentals = JSON.parse(File.read(Rails.root.join('db', 'rentals.json')))['data']
+
+rentals.each do |rental|
+  flat_attribute_names = %w[title owner city category bedrooms image description]
+
+  attribute_hash = flat_attribute_names.map do |name|
+    [name, rental['attributes'][name]]
+  end.to_h
+
+  attribute_hash['url_slug'] = rental['id']
+
+  %w[lat lng].each do |coordinate|
+    attribute_hash[coordinate] = rental['attributes']['location'][coordinate]
+  end
+
+  Rental.create(attribute_hash)
+end
